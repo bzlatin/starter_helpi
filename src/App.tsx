a6ea748997a16586366fc8235431c3232c75c167
Ben Zlatin <77761878+bzlatin@users.noreply.github.com>
2024-04-02 16:44:11 -0400

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,7 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <p>Ben</p>
+        <p>Bennn</p>
         <a
           className="App-link"
           href="https://reactjs.org"
