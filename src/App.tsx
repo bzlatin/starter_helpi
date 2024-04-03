31f17d37e4b074e8e4f794aca11c4fba9e6236ef
Christian Brandt <brandtc@gmail.com>
2024-04-03 18:34:27 -0400

    added last name on new branch

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <p>Ben zlatin</p>
-        <p>Christian</p>
+        <p>Christian Brandt</p>
         <a
           className="App-link"
           href="https://reactjs.org"
