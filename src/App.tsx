988453effb5e05ec4b2e8d587ac8e4cb6fadfe55
BENJAMIN ZLATIN <bzlatin@udel.edu>
2024-04-04 17:08:49 -0400

    fixed merge error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
+import logo from "./logo.svg";
 import "./App.css";
 import { Form } from "react-bootstrap";
-import { Button, TextInput, Pane, Heading, Button } from "evergreen-ui";
+import { Pane, Heading, Button } from "evergreen-ui";
 
 //local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
 let keyData = "";
@@ -17,8 +18,7 @@ function App() {
   //sets the local storage item to the api key the user inputed
   function handleSubmit() {
     localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again,
-    // I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
   }
 
   //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
@@ -128,5 +128,10 @@ function App() {
         </Form>
         <Button marginTop="10px" onClick={handleSubmit}>
           Submit
+        </Button>
+      </Pane>
+    </Pane>
+  );
+}
 
 export default App;
