4541fac00c485afd01205322a2715e53c579848e
BENJAMIN ZLATIN <bzlatin@udel.edu>
2024-04-04 17:10:49 -0400

    fixed lint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Form } from "react-bootstrap";
 import { Pane, Heading, Button } from "evergreen-ui";
