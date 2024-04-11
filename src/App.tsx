362b00102f144aaabf70aef05a659f1940f306c1
BENJAMIN ZLATIN <bzlatin@udel.edu>
2024-04-10 21:16:17 -0400

    fixed routing so it works in github pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Navigate,
+  HashRouter,
+} from "react-router-dom";
 import Home from "./Home";
 import BasicQuiz from "./BasicQuiz";
 import DetailedQuiz from "./DetailedQuiz";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Navigate replace to="/home" />} />
         <Route path="/home" element={<Home />} />
         <Route path="/basicQuestionPage" element={<BasicQuiz />} />
         <Route path="/detailedQuestionPage" element={<DetailedQuiz />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
