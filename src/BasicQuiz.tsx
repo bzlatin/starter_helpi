44532b8ce66ffd57d657c2cdc458db26862428b6
JoshuCW <joshwash@udel.edu>
2024-04-23 13:11:36 -0400

    Answering Questions should be good

--- src/BasicQuiz.tsx
+++ src/BasicQuiz.tsx
@@ -3,74 +3,91 @@ import "./App.css";
 import { Form } from "react-bootstrap";
 import { Pane, Button, HomeIcon, Radio, Heading } from "evergreen-ui";
 import { useNavigate } from "react-router-dom";
-//import Box from '@mui/material/Box';
-//import LinearProgress from '@mui/material/LinearProgress';
-
-//local storage and API Key: key should be entered in by the user and will be stored in local storage (NOT session storage)
-
-
 
 const questions = [
-  {text: "1. I enjoy working in a team more than working alone.", value: "Q1"},
-  {text: "2. When working on a project. I am most interested in: ", value: "Q2"},
-  {text: "3. I am passionate about helping others directly.", value: "Q3"},
-  {text: "4. I am drawn to tasks that are: ", value: "Q4"},
-  {text: "5. I prefer to follow instructions rather than create my own path.", value: "Q5"},
-  {text: "6. I am more interested in working: ", value: "Q6"},
-  {text: "7. I prefer jobs that offer a clear path for advancement.", value: "Q7"}
+  { text: "1. I enjoy working in a team more than working alone.", value: "Q1" },
+  { text: "2. When working on a project. I am most interested in: ", value: "Q2" },
+  { text: "3. I am passionate about helping others directly.", value: "Q3" },
+  { text: "4. I am drawn to tasks that are: ", value: "Q4" },
+  { text: "5. I prefer to follow instructions rather than create my own path.", value: "Q5" },
+  { text: "6. I am more interested in working: ", value: "Q6" },
+  { text: "7. I prefer jobs that offer a clear path for advancement.", value: "Q7" }
 ];
 
-//Scuffed way to do choices
-const choices1 = [{text: "True", value: "true"}, {text: "False", value: "false"}];
-const choices2 = [{text: "The planning and organization", value: "choice"}, {text: "The creative process", value: "choice"}, {text: "The problem solving aspects", value: "choice"}, {text: "The execution and details", value: "choice"}];
-const choices3 = [{text: "True", value: "true"}, {text: "False", value: "false"}]
-const choices4 = [{text: "Analytical and data-driven", value: "choice"}, {text: "Creative and Expressive", value: "choice"}, {text: "Technical and Hands-On", value: "choice"}, {text: "Interpersonal and Service-Oriented", value: "choice"}];
-const choices5 = [{text: "True", value: "true"}, {text: "False", value: "false"}];
-const choices6 = [{text: "Outdoors", value: "choice"}, {text: "In an office", value: "choice"}, {text: "From home", value: "choice"}, {text: "In a different places, traveling", value: "choice"}];
-const choices7 = [{text: "True", value: "true"}, {text: "False", value: "false"}];
-
-const choices = [choices1, choices2, choices3, choices4, choices5, choices6, choices7];
+const choices = [
+  [{ text: "True", value: "true" }, { text: "False", value: "false" }],
+  [
+    { text: "The planning and organization", value: "choice" },
+    { text: "The creative process", value: "choice" },
+    { text: "The problem solving aspects", value: "choice" },
+    { text: "The execution and details", value: "choice" }
+  ],
+  [{ text: "True", value: "true" }, { text: "False", value: "false" }],
+  [
+    { text: "Analytical and data-driven", value: "choice" },
+    { text: "Creative and Expressive", value: "choice" },
+    { text: "Technical and Hands-On", value: "choice" },
+    { text: "Interpersonal and Service-Oriented", value: "choice" }
+  ],
+  [{ text: "True", value: "true" }, { text: "False", value: "false" }],
+  [
+    { text: "Outdoors", value: "choice" },
+    { text: "In an office", value: "choice" },
+    { text: "From home", value: "choice" },
+    { text: "In a different places, traveling", value: "choice" }
+  ],
+  [{ text: "True", value: "true" }, { text: "False", value: "false" }]
+];
 
 let keyData = "";
 const saveKeyData = "MYKEY";
-const prevKey = localStorage.getItem(saveKeyData); //so it'll look like: MYKEY: <api_key_value here> in the local storage when you inspect
+const prevKey = localStorage.getItem(saveKeyData);
 if (prevKey !== null) {
   keyData = JSON.parse(prevKey);
 }
 
 function BasicQuiz() {
-  let navigate = useNavigate(); // Hook for navigation
+  const [answers, setAnswers] = useState<string[]>(new Array(questions.length).fill(""));
+  const [selectedChoiceIndices, setSelectedChoiceIndices] = useState<Array<number>>(new Array(questions.length).fill(null));
+  let navigate = useNavigate();
 
-  const [key, setKey] = useState<string>(keyData); //for api key input
-  
+  const [key, setKey] = useState<string>(keyData);
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
 
-  //Setting up for the next Button
   const handleNext = () => {
-    if(currentQuestionIndex < questions.length-1) {
+    if (currentQuestionIndex < questions.length - 1) {
       setCurrentQuestionIndex(currentQuestionIndex + 1);
     }
   }
-  //Setting up for the back button
+
   const handleBack = () => {
     if (currentQuestionIndex > 0) {
-      setCurrentQuestionIndex(currentQuestionIndex-1);
+      setCurrentQuestionIndex(currentQuestionIndex - 1);
     }
   };
 
-  
-  //sets the local storage item to the api key the user inputed
-  function handleSubmit() {
-    localStorage.setItem(saveKeyData, JSON.stringify(key));
-    window.location.reload(); //when making a mistake and changing the key again, I found that I have to reload the whole site before openai refreshes what it has stores for the local storage variable
+  const handleAnswerChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const updatedIndices = [...selectedChoiceIndices];
+    updatedIndices[currentQuestionIndex] = index;
+    setSelectedChoiceIndices(updatedIndices);
+
+    const updatedAnswers = [...answers];
+    updatedAnswers[currentQuestionIndex] = choices[currentQuestionIndex][index].value;
+    setAnswers(updatedAnswers);
   }
 
-  //whenever there's a change it'll store the api key in a local state called key but it won't be set in the local storage until the user clicks the submit button
+  const handleSubmit = () => {
+    localStorage.setItem(saveKeyData, JSON.stringify(key));
+    window.location.reload();
+  }
+
   function changeKey(event: React.ChangeEvent<HTMLInputElement>) {
     setKey(event.target.value);
   }
+
   const goToHomePage = () => {
-    navigate("/home"); // Use the navigate function
+    navigate("/home");
   };
 
   return (
@@ -104,8 +121,14 @@ function BasicQuiz() {
           {questions[currentQuestionIndex].text}
         </Heading>
         {choices[currentQuestionIndex].map((choice, index) => (
-    <Radio key={index} label={choice.text} value={choice.value} />
-  ))}
+          <Radio
+            key={index}
+            label={choice.text}
+            value={choice.value}
+            checked={selectedChoiceIndices[currentQuestionIndex] === index}
+            onChange={handleAnswerChange(index)}
+          />
+        ))}
       </Pane>
       <Pane display="flex" marginBottom={20}>
         <Button onClick={handleBack} disabled={currentQuestionIndex === 0}>Back</Button>
