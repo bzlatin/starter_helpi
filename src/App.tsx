89973c9c4a1aea92a0e99e4a1533837260d9177d
Christian Brandt <brandtc@gmail.com>
2024-04-23 16:57:17 -0400

    fixed homepage background color bug

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,12 @@ function App() {
   return (
     <HashRouter>
       <div
-        style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          minHeight: "100vh",
+          backgroundColor: "#F9FAFC",
+        }}
       >
         <Routes>
           <Route path="/" element={<Navigate replace to="/home" />} />
@@ -23,8 +28,8 @@ function App() {
             left: 0,
             bottom: 0,
             width: "100%",
-            backgroundColor: "#fff", // Add a background color if needed
-            boxShadow: "0 -2px 4px rgba(0, 0, 0, 0.1)", // Optional: Adds a shadow to the top of the footer
+            backgroundColor: "#FFFFFF",
+            boxShadow: "0 -1px 2px rgba(0, 0, 0, 0.1)", // Optional: Adds a shadow to the top of the footer
             zIndex: 1000,
           }}
         >
